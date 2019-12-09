69d6aa254b9e476399f7b2a7700b8d7e9615a0e5
tmilne1 <tmilne1@gmail.com>
2019-12-08 23:02:51 -0500

    added canvas, added drawing lines on canvas cued by mouse events

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log('hello')
+
+window.onload = function(){
+const canvas = document.querySelector('#draw');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+context.strokeStyle = '#BADA55';
+context.lineJoin = "round";
+context.lineCap ="round";
+
+let drawing = false;
+let lastX = 0;
+let lastY= 0;
+
+function draw(event){
+    if(!drawing){return}
+    console.log(event)
+    context.beginPath();
+    context.moveTo(lastX,lastY);
+    context.lineTo(event.offsetX,event.offsetY);
+    context.stroke();
+    lastX = event.offsetX;
+    lastY = event.offsetY;
+    
+}
+canvas.addEventListener('mousemove',draw)
+canvas.addEventListener('mouseup', () => drawing = false);
+canvas.addEventListener('mouseout', ()=> drawing = false);
+canvas.addEventListener('mousedown', (event) => {
+    lastX = event.offsetX;
+    lastY = event.offsetY;
+    drawing = true;
+});
+
+console.log("loaded")
+}
+  
